ccab2e20e54529d67372f9162ef4b41d7131810c
michaeldrotar <michaeldrotar@gmail.com>
2024-07-25 22:07:10 -0400

    Add pokedex tutorial

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/michaeldrotar/pokedexcli
+
+go 1.22.5
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"time"
+
+	"github.com/michaeldrotar/pokedexcli/internal/pokeapi"
+)
+
+func main() {
+	pokeClient := pokeapi.NewClient(5*time.Second, time.Minute*5)
+	cfg := &config{
+		caughtPokemon: map[string]pokeapi.Pokemon{},
+		pokeapiClient: pokeClient,
+	}
+
+	startRepl(cfg)
+}
